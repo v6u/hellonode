0f819d5ce991bbc875ea7f890fd504aaec4fbb74
IWINMATHEW <imathew@ecloucontrol.com>
2021-07-12 11:11:49 +0000

    hellonode for keycloak

--- src/index.js
+++ src/index.js
@@ -3,15 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import Keycloak from 'keycloak-js';
+//Get the keycloak configuration
+let keycloak = Keycloak('./resources/keycloak.json');
+//Initialization of the keycloak instance
+keycloak.init({ onLoad: 'login-required' }).success((authenticated) => {
+if (!authenticated) {
+window.location.reload();
+} else {
+console.info("Authenticated");
+}
+//React Render on authentication
+ReactDOM.render( <React.StrictMode>
+          <App />
+        </React.StrictMode>,
+document.getElementById('root'));
 serviceWorker.unregister();
+
+//store authentication tokens in sessionStorage for usage in app
+sessionStorage.setItem('authentication', keycloak.token);
+sessionStorage.setItem('refreshToken', keycloak.refreshToken);
+//to regenerate token on expiry
+setTimeout(() => {
+keycloak.updateToken(70).success((refreshed) => {
+if (refreshed) {
+console.debug('Token refreshed' + refreshed);
+} else {
+console.warn('Token not refreshed, valid for '
++ Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+}
+}).error(() => {
+console.error('Failed to refresh token');
+});
+}, 60000)
+}).error(() => {
+console.error("Authenticated Failed");
+});
