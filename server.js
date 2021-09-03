ac4627e963ff1a05b90e8e7b0c7998941a69b953
IWINMATHEW <imathew@ecloucontrol.com>
2021-09-03 10:04:20 +0000

    used promclient removing other npm packages

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-
 var ip = require("ip");
 
+const { collectDefaultMetrics, register } = require('prom-client');
+
+collectDefaultMetrics({
+	timeout: 10000,
+	gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5], // These are the default buckets.
+});
+
+console.log(register.metrics());
+
 var cors = require("cors");
-var prometheus = require('appmetrics-prometheus').attach();
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(cors());
 app.get('/message', (req, res) => {
@@ -33,4 +40,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
+// Setup server to Prometheus scrapes:
+
+app.get('/metrics', async (req, res) => {
+	try {
+		res.set('Content-Type', register.contentType);
+		res.end(await register.metrics());
+	} catch (ex) {
+		res.status(500).end(ex);
+	}
+});
+const port = process.env.PORT || 8080;
+console.log(
+	`Server listening to ${port}, metrics exposed on /metrics endpoint`,
+);
 app.listen(8080)
